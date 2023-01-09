d34d4e06fff80c0cb4cbd0ca562b288ddaf18905
Erwan <erwan.simonetti@epitech.eu>
2023-01-09 17:12:11 +0100

    feat(architecture): started to migrate from Makefile to Cmake + architecture with 2 binaries for servern and client side

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -9,6 +9,7 @@
 
 int main (int ac, char const *av[])
 {
+    std::cout << "server binary" << std::endl;
     std::cout << "ac = " << ac << std::endl;
     std::cout << "binary name = " << av[0] << std::endl;
     return 0;
