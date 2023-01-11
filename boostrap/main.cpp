a70b1977d9c07038e43ee233cc73ce7f4d6329bb
Timothée de Boynes <timothee.de-boynes@epitech.eu>
2023-01-11 14:45:06 +0100

    jaj
    Co-authored-by: 0Pixelle0 <JulietteDestang@users.noreply.github.com>
    Co-authored-by: BlanchoMartin <BlanchoMartin@users.noreply.github.com>

--- boostrap/main.cpp
+++ boostrap/main.cpp
@@ -16,123 +16,125 @@
 #include <string>
 #include <algorithm>
 
-class entity
-{
-public:
-    explicit entity(size_t id) : _id(id) {}
-    template <typename T, typename = std::enable_if_t<std::is_convertible_v<T, size_t>>>
-    explicit entity(T t) : _id(static_cast<size_t>(t)) {}
+class entity {
+    public:
+        explicit entity(size_t id) : _id(id) {}
+        template <typename T, typename = std::enable_if_t<std::is_convertible_v<T, size_t>>>
+        explicit entity(T t) : _id(static_cast<size_t>(t)) {}
 
-    operator size_t() const { return _id; }
+        operator size_t() const { return _id; }
 
-private:
-    size_t _id;
+    private:
+        size_t _id;
 };
 
 template <typename Component>
 class sparse_array
 {
-public:
-    using value_type = Component; // optional component type
-    using reference_type = value_type &;
-    using const_reference_type = value_type const &;
-    using container_t = std::vector<value_type>; // optionally add your allocator template here.
-    using size_type = typename container_t::size_type;
-    using iterator = typename container_t::iterator;
-    using const_iterator = typename container_t::const_iterator;
-
-public:
-    sparse_array() = default; // You can add more constructors .
-    sparse_array(sparse_array const &other)
-    {
-        this->_data = other._data;
-    }; // copy constructorn
-
-    sparse_array(sparse_array &&other) noexcept
-    {
-        this->_data = other._data;
-        other._data.clear();
-    }; // move constructor
-
-    ~sparse_array() = default;
-
-    sparse_array &operator=(sparse_array const &other)
-    {
-        this->_data = other._data;
-        return *this;
-    }; // copy assignment operator
-    sparse_array &operator=(sparse_array &&other) noexcept
-    {
-        this->_data = other._data;
-        return *this;
-    } // move assignment operator
-    reference_type operator[](size_t idx)
-    {
-        return _data[idx];
-    };
-
-    const_reference_type operator[](size_t idx) const
-    {
-        return _data[idx];
-    }
-    iterator begin() { return _data.begin(); };
-    const_iterator begin() const { return *_data.begin(); };
-    const_iterator cbegin() const { return *_data.begin(); };
-    iterator end() { return _data.end(); };
-    const_iterator end() const { return *_data.end(); };
-    const_iterator cend() const { return *_data.end(); };
-    size_type size() const
-    {
-        return _data.size();
-    };
-
-    reference_type insert_at(size_type pos, Component const &it)
-    {
-        if (_data.size() < pos + 1)
-            _data.resize(pos + 1);
-        _data[pos] = it;
-        return _data[pos];
-    };
-
-    reference_type insert_at(size_type pos, Component &&it)
-    {
-        if (_data.size() < pos + 1)
-            _data.resize(pos + 1);
-        _data[pos] = it;
-        return _data[pos];
-    };
-
-    // template <class ... Params >
-    // reference_type emplace_at ( size_type pos , Params &&...); // optional
-
-    void erase(size_type pos)
-    {
-        _data[pos] = 0;
-    };
-
-    size_type get_index(value_type const &it) const
-    {
-        auto elem = find(_data.begin(), _data.end(), it);
-
-        int index = -1;
-        if (elem != _data.end())
+    public:
+        using value_type = Component; // optional component type
+        using reference_type = value_type &;
+        using const_reference_type = value_type const &;
+        using container_t = std::vector<value_type>; // optionally add your allocator template here.
+        using size_type = typename container_t::size_type;
+        using iterator = typename container_t::iterator;
+        using const_iterator = typename container_t::const_iterator;
+    
+    public:
+        sparse_array() = default; // You can add more constructors .
+        sparse_array(sparse_array const &other)
         {
-            index = elem - _data.begin();
+            this->_data = other._data;
+        }; // copy constructorn
+    
+        sparse_array(sparse_array &&other) noexcept
+        {
+            this->_data = other._data;
+            other._data.clear();
+        }; // move constructor
+    
+        ~sparse_array() = default;
+    
+        sparse_array &operator=(sparse_array const &other)
+        {
+            this->_data = other._data;
+            return *this;
+        }; // copy assignment operator
+        sparse_array &operator=(sparse_array &&other) noexcept
+        {
+            this->_data = other._data;
+            return *this;
+        } // move assignment operator
+        reference_type operator[](size_t idx)
+        {
+            return _data[idx];
+        };
+    
+        const_reference_type operator[](size_t idx) const
+        {
+            return _data[idx];
         }
-        return index;
-    };
-
-    friend std::ostream &operator<<( std::ostream &output, const sparse_array &D ) {
-        for (size_t i = 0; i != D.size(); i++) {
-            output << D[i] << std::endl ;
+        iterator begin() { return _data.begin(); };
+        const_iterator begin() const { return *_data.begin(); };
+        const_iterator cbegin() const { return *_data.begin(); };
+        iterator end() { return _data.end(); };
+        const_iterator end() const { return *_data.end(); };
+        const_iterator cend() const { return *_data.end(); };
+    
+        size_type size() const
+        {
+            return _data.size();
+        };
+    
+        reference_type insert_at(size_type pos, Component const &it)
+        {
+            if (_data.size() < pos + 1)
+                _data.resize(pos + 1);
+            _data[pos] = it;
+            return _data[pos];
+        };
+    
+        reference_type insert_at(size_type pos, Component &&it)
+        {
+            if (_data.size() < pos + 1)
+                _data.resize(pos + 1);
+            _data[pos] = it;
+            return _data[pos];
+        };
+    
+        // template <class ... Params >
+        // reference_type emplace_at ( size_type pos , Params &&...); // optional
+    
+        void erase(size_type pos)
+        {
+            _data[pos] = 0;
+        };
+    
+        size_type get_index(value_type const &it) const
+        {
+            auto elem = find(_data.begin(), _data.end(), it);
+    
+            int index = -1;
+            if (elem != _data.end())
+            {
+                index = elem - _data.begin();
+            }
+            return index;
+        };
+    
+        friend std::ostream &operator<<( std::ostream &output, const sparse_array &D ) {
+            for (size_t i = 0; i != D.size(); i++) {
+                output << D[i] << std::endl ;
+            }
+            return output;            
         }
-        return output;            
-    }
-
-
-private:
-    container_t _data;
+    
+    
+    private:
+        container_t _data;
 };
+
+
 
 struct NamedType
 {
@@ -151,11 +153,16 @@ class registry
             static sparse_array<Component> newElem;
             _components_arrays.emplace(std::type_index(typeid(Component)), newElem);
             auto lambda = []  (registry &regis, entity const &it) -> void {
-                regis.remove_component<Component>(it);
+                regis.remove_component<Component>(it); 
             };
             _function_stored.push_back(lambda);
             return newElem;
         };
+
+        template <class Component>
+        void unregister_component(registry &reg, entity const &id) {
+            reg.get_components<Component>().erase(id);
+        }
 
         template <class Component>
         sparse_array<Component> &get_components() {
@@ -167,9 +174,16 @@ class registry
             return std::any_cast<sparse_array<Component>&>(_components_arrays.find(std::type_index(typeid(Component)))->second);
         };
         entity spawn_entity();
-        entity entity_from_index(std::size_t idx);
 
-        void kill_entity(entity const &e);
+        template <class Component>
+        entity entity_from_index(std::size_t idx) {
+            // return _components_arrays.find(std::type_index(typeid(Component)))->second[idx];
+        };
+
+        void kill_entity(entity const &e) {
+            _function_stored[0](*this, e);
+        };
+
         template <typename Component>
         typename sparse_array<Component>::reference_type add_component(entity const &to, Component &&c);
         template <typename Component, typename ... Params>
@@ -178,14 +192,15 @@ class registry
         template <typename Component>
         void remove_component(entity const &from) {
             sparse_array<Component> vec = std::any_cast<sparse_array<Component>>(_components_arrays.find(std::type_index(typeid(Component)))->second);
-            auto it = std::find(vec.begin(), vec.end(), from);
-            if (it != vec.end())
-                vec.erase(it - vec.begin());
+            // auto it = std::find(vec.begin(), vec.end(), from);
+            vec.erase(from._id);
+            // if (it != vec.end())
+            //     vec.erase(it - vec.begin());
         }
 
-    private:
         std::unordered_map<std::type_index, std::any> _components_arrays;
         std::vector<std::function<void(registry &, entity const &)>> _function_stored;
+    private:
 };
 
 int main(void)
@@ -206,12 +221,16 @@ int main(void)
     reg.get_components<char>().insert_at(1, 'a');
     reg.get_components<int>().insert_at(1, 1);
     reg.get_components<int>().insert_at(0, 2);
+
+    std::cout << "function stored size = " << reg._function_stored.size() << '\n';
     printf("chars: \n");
     std::cout << reg.get_components<char>();
     printf("___________________\nint: \n");
     std::cout << reg.get_components<int>();
 
-    reg.remove_component<char>(reg.get_components<char>()[0]);
+    // reg.remove_component<char>(reg.get_components<char>()[0]);
+    // reg.unregister_component<char>(reg, entity(1));
+    reg.    (entity(1));
     printf("__________________\nchars: \n");
     std::cout << reg.get_components<char>();
     // reg.remove_component()
