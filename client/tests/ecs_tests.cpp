319c73af0463539cc2d6865ab07566af1427aa04
MartinBl <martin.blancho@epitech.eu>
2023-01-23 16:29:13 +0100

    feat(update erase) with nullopt for remove a place in vector

--- client/tests/ecs_tests.cpp
+++ client/tests/ecs_tests.cpp
@@ -7,6 +7,30 @@
 
 #include <gtest/gtest.h>
 #include "registry.hpp"
+#include "Components.hpp"
+
+class registryTests: public ::testing::Test { 
+public: 
+   myTestFixture1( ) { 
+       // initialization code here
+   } 
+
+   void SetUp( ) { 
+       // code here will execute just before the test ensues 
+   }
+
+   void TearDown( ) { 
+       // code here will be called just after the test completes
+       // ok to through exceptions from here if need be
+   }
+
+   ~myTestFixture1( )  { 
+       // cleanup any pending stuff, but no exceptions allowed
+   }
+
+   // put in any custom data members that you need 
+};
+
 
 TEST(Step0, DefiningEntityAsSizeT) {
     entity a(0);
@@ -44,36 +68,38 @@ TEST(Step1, RegistryInsertAt) {
 TEST(Step1, RegistryErase) {
     registry reg;
 
-    int firstTest = 0;
-    int secondTest = -2147483647;
-    int thirdTest = 42;
+    Position pos1;
+    Position pos2;
+    Position pos3;
 
-    reg.register_component<int>();
-    reg.get_components<int>().insert_at(0, firstTest);
-    reg.get_components<int>().insert_at(1, secondTest);
-    reg.get_components<int>().insert_at(2, thirdTest);
-    std::cout << "INT BEFORE ERASE : " << std::endl;
-    std::cout << reg.get_components<int>()[0].value() << std::endl;
-    std::cout << reg.get_components<int>()[1].value() << std::endl;
-    std::cout << reg.get_components<int>()[2].value() << std::endl;
-    std::cout << "OVER" << std::endl;
-    EXPECT_EQ(reg.get_components<int>()[0].value(), firstTest);
-    EXPECT_EQ(reg.get_components<int>()[1].value(), secondTest);
-    EXPECT_EQ(reg.get_components<int>()[2].value(), thirdTest);
-    reg.get_components<int>().erase(1);
-    std::cout << "INT AFTER ERASE: " << std::endl;
-    std::cout << reg.get_components<int>()[0].value() << std::endl;
-    std::cout << reg.get_components<int>()[1].value() << std::endl;
-    std::cout << reg.get_components<int>()[2].value() << std::endl;
-    std::cout << "OVER" << std::endl;
-    EXPECT_EQ(reg.get_components<int>()[0].value(), firstTest);
-    EXPECT_EQ(reg.get_components<int>()[2].value(), thirdTest);
+    reg.register_component<Position>();
+    reg.get_components<Position>().insert_at(0, Position());
+    reg.get_components<Position>().insert_at(1, Position());
+    reg.get_components<Position>().insert_at(2, Position());
+    std::cout << "SPARSE ARRAY BEFORE ERASE : " << std::endl;
+    std::cout << reg.get_components<Position>();
+    int sizeBefore = reg.get_components<Position>().size();
+    reg.get_components<Position>().erase(1);
+    std::cout << "SPARSE ARRAY AFTER ERASE : " << std::endl;
+    std::cout << reg.get_components<Position>();
+    int sizeAfter = reg.get_components<Position>().size();
+    // EXPECT_EQ(reg.get_components<int>()[0].value(), firstTest);
+    // EXPECT_EQ(reg.get_components<int>()[1].value(), secondTest);
+    // EXPECT_EQ(reg.get_components<int>()[2].value(), thirdTest);
+    // reg.get_components<int>().erase(1);
+    // std::cout << reg.get_components<int>() << std::endl;
+    // // std::cout << reg.get_components<int>()[1].value() << std::endl;
+    // std::cout << reg.get_components<int>()[2].value() << std::endl;
+    // std::cout << "OVER" << std::endl;
+    // EXPECT_EQ(reg.get_components<int>()[0].value(), firstTest);
+    // EXPECT_EQ(reg.get_components<int>()[2].value(), thirdTest);
     try
     {
-        EXPECT_EQ(reg.get_components<int>()[1].value(), -1);
+        reg.get_components<Position>()[1].value();
     }
     catch(const std::exception& error) // this is expected because we're trying to see what's in an empty std::optional -> throws an error
     {
+        // std::cout << error.what() << std::endl;
         EXPECT_EQ(error.what(), "bad optional acces");
     }
     
