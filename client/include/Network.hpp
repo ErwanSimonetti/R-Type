a391592ee456e11d287ec436d5180d0d919fb65e
Timothée de Boynes <timothee.de-boynes@epitech.eu>
2023-01-29 22:54:58 +0100

    feat(network): Work in progress
    generalised the client and server to the class network and protocol
    
    still need to figure out how to reveive data properly
    
    Co-authored-by: 0Pixelle0 <JulietteDestang@users.noreply.github.com>

--- client/include/Network.hpp
+++ client/include/Network.hpp
@@ -5,32 +5,51 @@
 ** Network
 */
 
+#pragma once
+
 #ifndef NETWORK_HPP_
 #define NETWORK_HPP_
+
 
 #include <boost/asio.hpp>
 #include <boost/array.hpp>
 #include <iostream>
 #include <vector>
 
+#include "Protocol.hpp"
+
+struct jaj {
+    int lol;
+    int lil;
+};
+
 class Network {
     public:
-        Network(boost::asio::io_service& io_service);
+        Network(boost::asio::io_service &io_service, const std::string& host, const std::string& port);
+        Network(boost::asio::io_service& io_service, const std::string &port);
         ~Network();
         
         void addEndpoint(boost::asio::ip::udp::endpoint endpoint);
         
         template <typename Data>
-        void udpSend(char *buffer, boost::asio::ip::udp::endpoint endpoint);
+        void udpSend(char *buffer, boost::asio::ip::udp::endpoint endpoint) {
+            _socket.async_send_to(boost::asio::buffer(buffer, sizeof(Data)), endpoint,
+                [this, buffer, endpoint](boost::system::error_code /*ec*/, std::size_t /*bytes_sent*/) 
+            {
+                std::cerr << "Succefully sent: " << buffer << " to:" << endpoint << "\n";
+            });
+        }
 
-        void udpReceive();
+        void udpReceive(std::function<void()> func);
         
 
+        std::vector<boost::asio::ip::udp::endpoint> _endpoints;
     protected:
     private:
         boost::asio::ip::udp::socket _socket;
-        std::vector<boost::asio::ip::udp::endpoint> _endpoints;
         char _recvBuffer[1024];
+
+        Protocol _protocol;
 };
 
 #endif /* !NETWORK_HPP_ */
