2f2b9c2e7361a3d9ca0916ac315f884511cce6d9
MartinBl <martin.blancho@epitech.eu>
2023-01-30 16:39:05 +0100

    feat(add tests for sparse array): tests basic function

---
+++ client/tests/registry_tests.cpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** registry_tests
+*/
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <sstream>
+
+#include "registry.hpp"
+#include "entity.hpp"
+#include "System.hpp"
+
+TEST(Registry, RegisterComponent) {
+    registry res;
+    res.register_component<Position>();
+    res.register_component<Velocity>();
+
+    entity a = res.spawn_entity();
+    Position pos;
+    Velocity vel;
+    pos.build_component(1, 1);
+    vel.build_component(3, 4);
+
+
+    res.add_component(a, std::move(pos));
+    res.add_component(a, std::move(vel));
+    EXPECT_EQ(res.get_components<Position>().size(), 1);
+    EXPECT_EQ(res.get_components<Velocity>().size(), 1);
+    EXPECT_EQ(res.get_components<Position>().get_index(0).value()._x, 1);
+    EXPECT_EQ(res.get_components<Position>().get_index(0).value()._y, 1);
+}
