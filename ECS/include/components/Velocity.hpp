0c938a2f1ca64be37b06b7d93e4739e5a1f12579
Timothée de Boynes <timothee.de-boynes@epitech.eu>
2023-02-04 15:47:39 +0100

    feat: Refactored everything

--- ECS/include/components/Velocity.hpp
+++ ECS/include/components/Velocity.hpp
@@ -9,6 +9,7 @@
 
 struct Velocity {
     Velocity() = default;
+<<<<<<< HEAD
     void set_component(const uint16_t &velocityX, const uint16_t &velocityY, const uint16_t &speedX, const uint16_t &speedY) {
         _vX = velocityX;
         _vY = velocityY;
@@ -23,6 +24,17 @@ struct Velocity {
     friend std::ostream &operator<<(std::ostream &output, const Velocity &D) {
         output << "velocity x = " << D._vX << "velocity y = " <<  D._vY <<
         "speed x = " << D._speedX << "speed y = " << D._speedY << std::endl;
+=======
+    void build_component(const int16_t &vx, const int16_t &vy) {
+        _vx = vx;
+        _vy = vy;
+    }
+    int16_t _vx;
+    int16_t _vy;
+
+    friend std::ostream &operator<<(std::ostream &output, const Velocity &D) {
+        output << "x = " << D._vx << " y = " <<  D._vy << std::endl;
+>>>>>>> b364e23 (feat: Refactored everything)
         return output;
     }
 };
--- ECS/src/System.cpp
+++ ECS/src/System.cpp
@@ -5,6 +5,7 @@
 ** system
 */
 
+<<<<<<< HEAD
 #include <cstdlib>
 #include "System.hpp"
 #include "registry.hpp"
@@ -38,6 +39,11 @@ void followPathSystem(const sparse_array<Position> &positions, sparse_array<Velo
     }
 }
 
+=======
+#include "System.hpp"
+#include "registry.hpp"
+
+>>>>>>> b364e23 (feat: Refactored everything)
 void logging_system (sparse_array<Position> const& positions, sparse_array<Velocity> const& velocities) {
 
     for (size_t i = 0; i < positions.size() && i < velocities.size() ; ++i) {
@@ -46,11 +52,16 @@ void logging_system (sparse_array<Position> const& positions, sparse_array<Veloc
 
         if ( pos && vel ) {
             std :: cerr << i << ": Position = { " << pos.value()._x << ", " << pos.value()._y
+<<<<<<< HEAD
             << " } , Velocity = { " << vel.value()._vX << ", " << vel.value()._vY << " }" << std::endl;
+=======
+            << " } , Velocity = { " << vel.value()._vx << ", " << vel.value()._vy << " }" << std::endl;
+>>>>>>> b364e23 (feat: Refactored everything)
         }
     }
 }
 
+<<<<<<< HEAD
 void position_system(sparse_array<Position> &positions, sparse_array<Velocity> &velocities) {
     for (size_t i = 0; i < positions.size() && i < velocities.size(); ++ i) {
         auto &pos = positions[i];
@@ -60,6 +71,15 @@ void position_system(sparse_array<Position> &positions, sparse_array<Velocity> &
             pos.value()._y += vel.value()._vY;
             vel.value()._vX = 0;
             vel.value()._vY = 0;
+=======
+void position_system(sparse_array<Position> &positions, const sparse_array<Velocity> &velocities) {
+    for (size_t i = 0; i < positions.size() && i < velocities.size(); ++ i) {
+        auto &pos = positions[i];
+        auto const &vel = velocities[i];
+        if (pos && vel) {
+            pos.value()._x += vel.value()._vx;
+            pos.value()._y += vel.value()._vy;
+>>>>>>> b364e23 (feat: Refactored everything)
         }
     }
 }
@@ -69,8 +89,13 @@ EntityEvent control_system(registry &r, std::vector<int> &directions, sparse_arr
     EntityEvent entityEvent;
     entityEvent.entity = -1;
     int current_direction = 0;
+<<<<<<< HEAD
     int16_t xDirection = 0;
     int16_t yDirection = 0;
+=======
+    int x_velocity = 0;
+    int y_velocity = 0;
+>>>>>>> b364e23 (feat: Refactored everything)
     for (size_t i = 0; i < velocities.size() && i < controllables.size() && i < positions.size(); ++ i) {
         auto &vel = velocities[i];
         auto &pos = positions[i];
@@ -82,6 +107,7 @@ EntityEvent control_system(registry &r, std::vector<int> &directions, sparse_arr
                 contr.value()._current_action = current_direction;
                 switch (current_direction) {
                     case KEYBOARD::ARROW_UP:
+<<<<<<< HEAD
                         yDirection = -1; // FIXME: should be 1 and not minus
                         entityEvent.events.emplace_back(GAME_EVENT::UP);
                         break;
@@ -100,15 +126,52 @@ EntityEvent control_system(registry &r, std::vector<int> &directions, sparse_arr
                     default:
                         xDirection = 0;
                         yDirection = 0;
+=======
+                        y_velocity = -1; // FIXME: should be 1 and not minus
+                        entityEvent.events.emplace_back(GAME_EVENT::UP);
+                        break;
+                    case KEYBOARD::ARROW_LEFT:
+                        x_velocity = -1;
+                        entityEvent.events.emplace_back(GAME_EVENT::LEFT);
+                        break;
+                    case KEYBOARD::ARROW_RIGHT:
+                        x_velocity = 1;
+                        entityEvent.events.emplace_back(GAME_EVENT::RIGHT);
+                        break;
+                    case KEYBOARD::ARROW_DOWN:
+                        y_velocity = 1;
+                        entityEvent.events.emplace_back(GAME_EVENT::RIGHT);
+                        break;
+                    default:
+                        x_velocity = 0;
+                        y_velocity = 0;
+>>>>>>> b364e23 (feat: Refactored everything)
                         break;
                 }
             }
             if (directions.empty()) {
+<<<<<<< HEAD
                 xDirection = 0;
                 yDirection = 0;
             }
             vel.value().set_component(xDirection * vel.value()._speedX, yDirection * vel.value()._speedY, vel.value()._speedX, vel.value()._speedY);
         }
     }
+=======
+                x_velocity = 0;
+                y_velocity = 0;
+            }
+            vel.value().build_component(x_velocity, y_velocity);
+        }
+    }
+    // for (size_t i = 0; i < velocities.size() && i < positions.size(); ++ i) {
+    //     auto &vel = velocities[i];
+    //     auto &contr = controllables[i];
+    //     auto &pos = positions[i];
+    //     if (vel && !contr && pos.value()._x <= 1900) {
+    //         vel.value().build_component(1, 0);
+    //     }
+    // }
+>>>>>>> b364e23 (feat: Refactored everything)
     return entityEvent;
 }
