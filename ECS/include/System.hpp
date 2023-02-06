68b428f896961c68cf24690e845be81be205f31b
Azzzen <axel.zenine@epitech.eu>
2023-02-07 00:30:15 +0100

    feat(projectiles): added hitbox and collision detection hitbox.hpp: added hitbox component engine.cpp : added registration of hbx system.c/hpp : created a collision detection system
    Co-authored-by: BlanchoMartin <BlanchoMartin@users.noreply.github.com>

--- ECS/include/System.hpp
+++ ECS/include/System.hpp
@@ -16,10 +16,12 @@
 #include "Position.hpp"
 #include "Controllable.hpp"
 #include "Pet.hpp"
+#include "Hitbox.hpp"
 #include "Drawable.hpp"
 #include "FollowPath.hpp"
 #include "Engine_utils.hpp"
 
+void collision_system(sparse_array<Position> &positions, sparse_array<Hitbox> &hitboxes);
 void logging_system (sparse_array<Position> const& positions, sparse_array<Velocity> const& velocities);
 void position_system(sparse_array<Position> &positions, sparse_array<Velocity> &velocities);
 EntityEvent control_system(registry &r, std::vector<int> &directions, sparse_array<Position> &positions, sparse_array<Controllable> &controllables, sparse_array<Velocity> &velocities);
