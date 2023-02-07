811b30d45635502943fdb4bb35d17980cec7ab25
Geekosaure <juliette.destang@epitech.eu>
2023-02-08 00:29:01 +0100

    feat(graphical): add some sprie for the background, spaceship and projectile

---
+++ ECS/include/components/Parallax.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** Animatable
+*/
+
+
+#pragma once
+
+// #include "../entity.hpp"
+#include <SFML/Graphics.hpp>
+
+struct Parallax {
+    Parallax() = default;
+
+    void set_component(const int16_t &endPos, const int16_t &startPos) {
+        _endPos = endPos;
+        _startPos = startPos;
+    }
+    int16_t _endPos;
+    int16_t _startPos;
+};
