b3e08ca88682e4197882f7178a9d91549883e5f8
Timothée de Boynes <timothee.de-boynes@epitech.eu>
2023-02-16 11:38:16 +0100

    feat(ECS): Cleaned the ECS also fixed a crash when 2 or + client were connected

--- client/include/Engine.hpp
+++ client/include/Engine.hpp
@@ -44,34 +44,24 @@ class Engine {
          */
         registry &get_registry();
 
-        /** 
-         * @brief Function used to create a friendly "character" entity, giving it an id, and various parameters 
-         * @param newEntity Entity ID
-         * @param velX int16_t corresponding to the vertical velocity
-         * @param velY int16_t corresponding to the horizontal velocity
-         * @param posX uint16_t corresponding to the vertical position
-         * @param posY uint16_t corresponding to the horizontal position
-         **/
+        /// @brief Function used to create a friendly "character" entity, giving it an id, and various parameters 
+        /// @param id Entity ID
+        /// @param col sf::Color object, until we use actual sprites
+        /// @param velX uint_16_t corresponding to the vertical velocity
+        /// @param velY uint_16_t corresponding to the horizontal velocity
+        /// @param posX uint_16_t corresponding to the vertical position
+        /// @param posY uint_16_t corresponding to the horizontal position
         void create_player(entity newEntity, const int16_t velX, const int16_t velY, const uint16_t posX, const uint16_t posY);
         
-        /**
-         * @brief Function used to create an enemy "character" entity, giving it an id, and various parameters 
-         * @param newEntity Entity ID, has to be unused
-         * @param velX int16_t corresponding to the vertical velocity
-         * @param velY int16_t corresponding to the horizontal velocity
-         * @param posX uint16_t corresponding to the vertical position
-         * @param posY uint16_t corresponding to the horizontal position
-         **/
+        /// @brief Function used to create an enemy "character" entity, giving it an id, and various parameters 
+        /// @param id Entity ID, has to be unused
+        /// @param col sf::Color object, until we use actual sprites
+        /// @param velX uint_16_t corresponding to the vertical velocity
+        /// @param velY uint_16_t corresponding to the horizontal velocity
+        /// @param posX uint_16_t corresponding to the vertical position
+        /// @param posY uint_16_t corresponding to the horizontal position
         void create_enemy_entity(entity newEntity, const int16_t velX, const int16_t velY, const uint16_t posX, uint16_t posY);
 
-        /** 
-         * @brief Function used to create an entity, giving it an id, and various parameters.
-         * @param newEntity Entity ID, has to be unused
-         * @param velX int16_t corresponding to the vertical velocity
-         * @param velY int16_t corresponding to the horizontal velocity
-         * @param posX uint16_t corresponding to the vertical position
-         * @param posY uint16_t corresponding to the horizontal position
-         */
         void create_entity(entity newEntity, const int16_t velX, const int16_t velY, const uint16_t posX, const uint16_t posY);
 
         /// @brief Generate a projectile using the id of a previously generated ship entity
