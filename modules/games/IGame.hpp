bf796e99ce02bccc2c57b9ba9bf2f8517a66cac8
Timothée de Boynes <timothee.de-boynes@epitech.eu>
2023-02-27 19:28:48 +0100

    fix(GameEngine): Remove useless functions from interface

--- modules/games/IGame.hpp
+++ modules/games/IGame.hpp
@@ -63,52 +63,6 @@ class IGame {
          */
         virtual void updateRegistry(registry &r, const GameData data[4]) = 0;
 
-
-        /** 
-         * @brief Function used to create a friendly "character" entity, giving it an id, and various parameters 
-         * @param r registry the registery to update
-         * @param newEntity Entity ID
-         * @param velX int16_t corresponding to the vertical velocity
-         * @param velY int16_t corresponding to the horizontal velocity
-         * @param posX uint16_t corresponding to the vertical position
-         * @param posY uint16_t corresponding to the horizontal position
-         **/
-        virtual void create_player(registry &r, entity newEntity, bool isControllable, const int16_t velX, const int16_t velY, const uint16_t posX, const uint16_t posY) = 0;
-
-        /**
-         * @brief Function used to create an enemy "character" entity, giving it an id, and various parameters 
-         * @param r registry the registery to update
-         * @param newEntity Entity ID, has to be unused
-         * @param velX int16_t corresponding to the vertical velocity
-         * @param velY int16_t corresponding to the horizontal velocity
-         * @param posX uint16_t corresponding to the vertical position
-         * @param posY uint16_t corresponding to the horizontal position
-         **/
-        virtual void create_enemy_entity(registry &r, entity newEntity, const int16_t velX, const int16_t velY, const uint16_t posX, uint16_t posY) = 0;
-
-        /** 
-         * @brief Function used to create an entity, giving it an id, and various parameters.
-         * @param r registry the registery to update
-         * @param newEntity Entity ID, has to be unused
-         * @param velX int16_t corresponding to the vertical velocity
-         * @param velY int16_t corresponding to the horizontal velocity
-         * @param posX uint16_t corresponding to the vertical position
-         * @param posY uint16_t corresponding to the horizontal position
-         */
-        virtual void create_entity(registry &r, entity newEntity, const int16_t velX, const int16_t velY, const uint16_t posX, const uint16_t posY) = 0;
-
-        /**
-         * @brief Generate a projectile using the id of a previously generated ship entity
-         * @param r registry the registery to update
-         * @param newEntity Entity ID, has to be unused
-         * @param parentId id of the parent ship
-         * @param col color
-         * @param velX x velocity
-         * @param velY y velocity
-         * @return a projectile entity 
-        */
-        virtual void create_projectile(registry &r, entity newEntity, int16_t parentId, const uint16_t velX, const uint16_t velY) = 0;
-
     protected:
     private:
 };
--- modules/games/RTYPE_game/include/Rtype.hpp
+++ modules/games/RTYPE_game/include/Rtype.hpp
@@ -22,9 +22,50 @@ class Rtype : public IGame {
         void run_gameLogic(registry &r, const Events &events);
         void updateRegistry(registry &r, const GameData &data);
         void updateRegistry(registry &r, const GameData data[4]);
+        
+        /** 
+         * @brief Function used to create a friendly "character" entity, giving it an id, and various parameters 
+         * @param r registry the registery to update
+         * @param newEntity Entity ID
+         * @param velX int16_t corresponding to the vertical velocity
+         * @param velY int16_t corresponding to the horizontal velocity
+         * @param posX uint16_t corresponding to the vertical position
+         * @param posY uint16_t corresponding to the horizontal position
+         **/
         void create_player(registry &r, entity newEntity, bool isControllable, const int16_t velX, const int16_t velY, const uint16_t posX, const uint16_t posY);
+
+        /**
+         * @brief Function used to create an enemy "character" entity, giving it an id, and various parameters 
+         * @param r registry the registery to update
+         * @param newEntity Entity ID, has to be unused
+         * @param velX int16_t corresponding to the vertical velocity
+         * @param velY int16_t corresponding to the horizontal velocity
+         * @param posX uint16_t corresponding to the vertical position
+         * @param posY uint16_t corresponding to the horizontal position
+         **/
         void create_enemy_entity(registry &r, entity newEntity, const int16_t velX, const int16_t velY, const uint16_t posX, uint16_t posY);
+
+        /** 
+         * @brief Function used to create an entity, giving it an id, and various parameters.
+         * @param r registry the registery to update
+         * @param newEntity Entity ID, has to be unused
+         * @param velX int16_t corresponding to the vertical velocity
+         * @param velY int16_t corresponding to the horizontal velocity
+         * @param posX uint16_t corresponding to the vertical position
+         * @param posY uint16_t corresponding to the horizontal position
+         */
         void create_entity(registry &r, entity newEntity, const int16_t velX, const int16_t velY, const uint16_t posX, const uint16_t posY);
+
+        /**
+         * @brief Generate a projectile using the id of a previously generated ship entity
+         * @param r registry the registery to update
+         * @param newEntity Entity ID, has to be unused
+         * @param parentId id of the parent ship
+         * @param col color
+         * @param velX x velocity
+         * @param velY y velocity
+         * @return a projectile entity 
+        */
         void create_projectile(registry &r, entity newEntity, int16_t parentId, const uint16_t velX, const uint16_t velY);
         void create_parallax(registry &r, entity newEntity, const uint16_t posX, const uint16_t posY, const uint16_t speed, const OBJECT obj);
 
