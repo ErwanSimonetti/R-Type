0a179b5dcbffd956a35fe33a95e2d26303843b0b
MartinBl <martin.blancho@epitech.eu>
2023-03-04 16:52:58 +0100

    fix(add begin implementation of kick)

---
+++ network/include/DeleteEntities.hpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2023
+** R-Type
+** File description:
+** DeleteEntities
+*/
+
+#ifndef DELETEENTITIES_HPP_
+#define DELETEENTITIES_HPP_
+
+struct DeleteEntities
+{
+    int _id;
+};
+
+#endif /* !DELETEENTITIES_HPP_ */
